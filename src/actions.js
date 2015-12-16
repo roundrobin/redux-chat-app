0e98be82c4d5a748444d06c4556d885c85da5d42
roundrobin <nils@five.com>
2015-12-16 12:42:10 +0100

    Adds test folder

--- src/actions.js
+++ src/actions.js
@@ -5,3 +5,14 @@ export let sayActionCreator = function (message) {
         message
     }
 }
+
+export let asyncSayActionCreator_1 = function (message) {
+    return function (dispatch) {
+        setTimeout(function () {
+            dispatch({
+                type: 'SAY',
+                message
+            })
+        }, 2000)
+    }
+}
---
+++ test/simple-test.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('immutability', () => {
+
+    it('is immutable', () => {
+      expect(1).to.equal(0);
+    });
+});
